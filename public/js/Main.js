625068adf0631b55a6492e0847257b76c7b630ed
Laermist <108534710+Laermist@users.noreply.github.com>
2022-07-05 07:37:30 +0800

    Update Main.js

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,49 +1,161 @@
-function action(){
-    document.querySelector('.add-comment').style.display = 'flex';
-};
+const db = require('../models/DML.js');
+const bcrypt = require(`bcrypt`);
+const UserModel = require('../models/UserModel.js');
+const PostModel = require('../models/PostModel.js');
+const CommentModel = require('../models/CommentModel.js');
 
-function closing(){
-    document.querySelector('.add-comment').style.display = 'none';
-}
+const controller = {
 
-function addPost(sess){
-    alert('Posting...');
-    console.log("Posting...");
-    var entry = {
-        userID: sess.userID,
-        username: sess.username,
-        content: $("#txtArea").val(),
-        date: getDate(),
-        category: $("#filter").val(),
-        likes: 0,
-        comments: 0
-    };
-    console.log("checking...");
-    if(entry.content.trim().length==0 || entry.category.trim().length==0){
-        alert("Make sure you have something written and a category selected.");
-        console.log("Make sure you have something written and a category selected.");
-        return false;
-    }
-    else
-    {
-        console.log("else...");
-        $.get('/add', entry, function (result) {
-            console.log(result);
+    getFavicon: function (req, res) {
+        res.status(204);
+    },
+
+    getIndex: function(req, res) {
+        res.render('Intro'); // This is to load the page initially
+        
+    },
+
+    getLogin: function(req, res) {
+        res.render('Login');
+    },
+
+    getMain: function(req, res) {
+        db.findMany(PostModel, {}, 'userID username content date likes comments', function (result) {
+            if(result)
+			{
+                console.log('Loading posts.');
+                console.log(result);
+                //res.render('../views/partials/feed',{userID:result.user, content:result.content, date:result.date, likes:result.likes, comments:result.comments});
+                db.findMany(CommentModel, {}, 'commentID userID username postID content', function (result2) {
+                    if(result2)
+                    {
+                        console.log('Loading comments.');
+                        console.log(result2);
+                        res.render('Main',{"Session": req,"Posts": result,"Comments": result2,}); // This is to load the page initially
+                    }
+                    else
+                    {
+                        res.render('Main');
+                    }
+                });
+                
+            }
+			else
+			{
+                res.render('Main');
+            }
+        });
+    },
+
+    getMainProfile: function(req, res) {
+        res.render('Main-profile');
+    },
+
+    getMainGuest: function(req, res) {
+        res.render('Main-Guest');
+    },
+
+    getReg: function(req, res) {
+        res.render('Register');
+    },
+
+    getTNC: function(req, res) {
+        res.render('TermsNConditions');
+    },
+
+    getPost: function(req, res) {
+        db.findOne(PostModel, {postID: req.query.postID}, 'postID', function (result)
+		{
+            res.send(result);
+        });
+    },
+
+    getAddAcc: function(req, res) {
+        UserModel.findOne({}, {}, function(error, result) {
+            var max_id = 100
+             if(result){
+                 max_id=result.userID +1;
+             }
+             bcrypt.hash(req.query.pwrd1,10).then(hash=>{
+                var user = {
+                userID: max_id,
+                name: req.query.uname,
+                email: req.query.email,
+                pword: hash
+                }
+                db.insertOne(UserModel, user, function(result)
+                {
+                    res.send(result);
+                });
+            }).catch(err=>{
+                console.log(err)
+            });
+            
+         }).sort({userID:-1}).limit(1);
+    },
+
+    getAcc: function(req,res) {
+        var user = null;
+        db.findOne(UserModel, {email: req.query.email}, 'name email pword', function (result)
+		{
             if(result)
             {
-                $('#txtArea').val('');
-                $('#filter').val('');
-
-                alert("Your post has been posted!");
-                return true;
+                bcrypt.compare(req.query.pword, result.pword, function(err,result2){
+                    if(result2)
+                    {
+                        user = {name:result.name, email:result.email};
+                        req.session.user = user;
+                        console.log(req.session);       // remove later on, for debugging purposes only
+                        res.send({message:"success",name:result.name});
+                    }
+                    else
+                    {
+                        req.session.user = user;
+                        res.send({message:"incorrect"});
+                    }
+                });
             }
             else
             {
-                alert('Function error encountered.');
-                console.log('Function error encountered.');
-                return false;
+                req.session.user = user;
+                res.send({message:"non-existing"});
             }
         });
+    },
+
+    getAdd: function(req, res) {
+        // your code here
+        PostModel.findOne({}, {}, function(error, result) {
+            var max_id = 0;
+                if(result){
+                    max_id=result.postID +1;
+                }
+                var post = {
+                    postID: max_id + userID*10000,
+                    userID: req.query.userID,
+                    username: req.query.username,
+                    content: req.query.content,
+                    date: req.query.date,
+                    category: req.query.category,
+                    likes: req.query.likes,
+                    comments: req.query.comments
+                }
+                db.insertOne(PostModel, post, function(result)
+                {
+                    res.send(result);
+                });
+            }).catch(err=>{
+                console.log(err)
+         }).sort({userID:-1}).limit(1);
+    },
+
+    getDelete: function (req, res) {
+        db.deleteOne(PostModel,{postid:req.query.postid},function(result)
+		{
+            res.send(result);
+        });
     }
+
 }
 
+module.exports = controller;
