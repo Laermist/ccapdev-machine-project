3300e2d9f4ed3ce0c5f0ba0e0d0a99de0e66ac11
Laermist <108534710+Laermist@users.noreply.github.com>
2022-07-05 07:36:48 +0800

    Update Main.js

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,11 +1,3 @@
-function show(){
-    document.querySelector('.show-comment').style.display = 'flex';
-}
-
-function exit(){
-    document.querySelector('.show-comment').style.display = 'none';
-}
-
 function action(){
     document.querySelector('.add-comment').style.display = 'flex';
 };
@@ -14,9 +6,44 @@ function closing(){
     document.querySelector('.add-comment').style.display = 'none';
 }
 
-$(document).ready(function () {
-    $('#createPost').submit(function(){
-        alert('Posting...');
-    });
-});
+function addPost(sess){
+    alert('Posting...');
+    console.log("Posting...");
+    var entry = {
+        userID: sess.userID,
+        username: sess.username,
+        content: $("#txtArea").val(),
+        date: getDate(),
+        category: $("#filter").val(),
+        likes: 0,
+        comments: 0
+    };
+    console.log("checking...");
+    if(entry.content.trim().length==0 || entry.category.trim().length==0){
+        alert("Make sure you have something written and a category selected.");
+        console.log("Make sure you have something written and a category selected.");
+        return false;
+    }
+    else
+    {
+        console.log("else...");
+        $.get('/add', entry, function (result) {
+            console.log(result);
+            if(result)
+            {
+                $('#txtArea').val('');
+                $('#filter').val('');
+
+                alert("Your post has been posted!");
+                return true;
+            }
+            else
+            {
+                alert('Function error encountered.');
+                console.log('Function error encountered.');
+                return false;
+            }
+        });
+    }
+}
 
